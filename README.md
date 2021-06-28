8a1efb393b30dcaf37de53cd6854a9d414d9ab1c
Andrei Dmitrijevõhh <andrei.dmitrijevohh@uptime.eu>
2021-06-28 16:59:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Steps
+
+1. Clone the repo
+2. Select the branch you want
+3. Run `npm i` to install dependencies
+4. Run one of the following commands, depending on intent:
+
+### Production Build
+
+```bash
+npm run build
+```
+
+### Development Build
+
+```bash
+npm run build-dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const mode = process.env.NODE_ENV || "development";
+const target = process.env.NODE_ENV === "production" ? "browserslist" : "web";
+
+module.exports = {
+  mode: mode,
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [{
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+    },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+    }],
+  },
+  target: target,
+  devtool: "source-map",
+  devServer: { contentBase: "./dist" },
+};
